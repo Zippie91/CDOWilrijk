3948fe4c4c825bfc1b020aaacb0920b1e853c84e
Raf Smits <raf_smits@hotmail.com>
2015-03-31 09:34:47 +0200

    Prepare functions.php for modal formatting

--- Scripts/functions.php
+++ Scripts/functions.php
@@ -4,15 +4,15 @@ function PrijsBerekening() {
 	//Soepen
 	<?php for($i = 0; $i < sizeof($soepen); $i++){ ?>
 		var val = 0;
-		val = $("#aantal_soep-<?php echo $i+1; ?>").val() * <?php echo $soepen[$i]["prijs"];?>;
+		val = $("#aantal-soep-<?php echo $i+1; ?>").val() * <?php echo $soepen[$i]["prijs"];?>;
 		$('#totaal-soep-<?php echo $i+1; ?>').text( "€ " + val.toFixed(2) );
 		totaal += val;
 	<?php } ?>
-  
+
   	//Voorgerechten
 	<?php for($i = 0; $i < sizeof($voorgerechten); $i++){ ?>
 		var val = 0;
-		val = $("#aantal_voorgerecht-<?php echo $i+1; ?>").val() * <?php echo $voorgerechten[$i]["prijs"];?>;
+		val = $("#aantal-voorgerecht-<?php echo $i+1; ?>").val() * <?php echo $voorgerechten[$i]["prijs"];?>;
 		$('#totaal-voorgerecht-<?php echo $i+1; ?>').text( "€ " + val.toFixed(2) );
 		totaal += val;
 	<?php } ?>
@@ -20,43 +20,43 @@ function PrijsBerekening() {
   	//Hoofdgerechten
 	<?php for($i = 0; $i < sizeof($hoofdgerechten); $i++){ ?>
 		var val = 0;
-		val = $("#aantal_hoofdgerecht-<?php echo $i+1; ?>").val() * <?php echo $hoofdgerechten[$i]["prijs"];?>;
+		val = $("#aantal-hoofdgerecht-<?php echo $i+1; ?>").val() * <?php echo $hoofdgerechten[$i]["prijs"];?>;
 		$('#totaal-hoofdgerecht-<?php echo $i+1; ?>').text( "€ " + val.toFixed(2) );
 		totaal += val;
 	<?php } ?>
-  
+
   	//Kindermenu
 	<?php for($i = 0; $i < sizeof($kindermenu); $i++){ ?>
 		var val = 0;
-		val = $("#aantal_kindermenu-<?php echo $i+1; ?>").val() * <?php echo $kindermenu[$i]["prijs"];?>;
+		val = $("#aantal-kindermenu-<?php echo $i+1; ?>").val() * <?php echo $kindermenu[$i]["prijs"];?>;
 		$('#totaal-kindermenu-<?php echo $i+1; ?>').text( "€ " + val.toFixed(2) );
 		totaal += val;
 	<?php } ?>
-  
+
   	//Pasta
 	<?php for($i = 0; $i < sizeof($pasta); $i++){ ?>
 		var val = 0;
-		val = $("#aantal_pasta-<?php echo $i+1; ?>").val() * <?php echo $pasta[$i]["prijs"];?>;
+		val = $("#aantal-pasta-<?php echo $i+1; ?>").val() * <?php echo $pasta[$i]["prijs"];?>;
 		$('#totaal-pasta-<?php echo $i+1; ?>').text( "€ " + val.toFixed(2) );
 		totaal += val;
 	<?php } ?>
-  
+
   	//Maaltijdsalades
 	<?php for($i = 0; $i < sizeof($maaltijdsalades); $i++){ ?>
 		var val = 0;
-		val = $("#aantal_maaltijdsalades-<?php echo $i+1; ?>").val() * <?php echo $maaltijdsalades[$i]["prijs"];?>;
+		val = $("#aantal-maaltijdsalades-<?php echo $i+1; ?>").val() * <?php echo $maaltijdsalades[$i]["prijs"];?>;
 		$('#totaal-maaltijdsalades-<?php echo $i+1; ?>').text( "€ " + val.toFixed(2) );
 		totaal += val;
 	<?php } ?>
-  
+
     //Desserts
 	<?php for($i = 0; $i < sizeof($desserts); $i++){ ?>
 		var val = 0;
-		val = $("#aantal_desserts-<?php echo $i+1; ?>").val() * <?php echo $desserts[$i]["prijs"];?>;
+		val = $("#aantal-desserts-<?php echo $i+1; ?>").val() * <?php echo $desserts[$i]["prijs"];?>;
 		$('#totaal-desserts-<?php echo $i+1; ?>').text( "€ " + val.toFixed(2) );
 		totaal += val;
-	<?php } ?>  
-	
+	<?php } ?>
+
     //totaal
 	$('#totaal').text( "€ " + totaal.toFixed(2) );
 	$('#hiddentotaal').val( totaal );
