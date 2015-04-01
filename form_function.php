06503379c2012d62b08b0e70fdc0fbcb86d4a0d5
lightlessfilms <lightlessfilms@gmail.com>
2015-04-01 18:25:05 +0200

    removed mail-pw

--- form_function.php
+++ form_function.php
@@ -33,7 +33,7 @@
       $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
       $mail->SMTPAuth = true;                               // Enable SMTP authentication
       $mail->Username = 'yannicktest.test@gmail.com';                 // SMTP username
-      $mail->Password = 'cdowilrijk123';                           // SMTP password
+      $mail->Password = '';                           // SMTP password
       $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
       $mail->Port = 465;                                    // TCP port to connect to
 
@@ -109,39 +109,6 @@
               $maaltijdsalade . "<br>" .
               $dessert . "<br>";
 
-/*
-              "<br><h2>Bestelde soepen: </h2>" . "<b>Wortel/kokossoepje met curry en koriander: </b>" . " " . $_POST["aantal-soep-1"] .
-              "<br><b>Basilicumsoepje met tartaar van mozzarella en tomaat: </b>" . $_POST["aantal-soep-2"] .
-              "<br><b>Tomatenvelouté met balletjes: </b>" . $_POST["aantal-soep-3"] .
-
-              "<br><h2>Bestelde voorgerechten: </h2>" . "<b>Carpaccio van sint-jacobsvruchten met tuinkruiden en limoendressing: </b>" . " " . $_POST["aantal-voorgerecht-1"] .
-              "<br><b>Huisgemaakte geitenkaaskroketjes met een fris slaatje: </b>" . $_POST["aantal-voorgerecht-2"] .
-              "<br><b>Vers geplukte lenteblaadjes met gevulde zalmbonbon: </b>" . $_POST["aantal-voorgerecht-3"] .
-
-              "<br><h2>Bestelde hoofdgerechten: </h2>" . "<b>Scampi's met appel/ananas/banaan/curry/kokossausje en wilde rijst: </b>" . " " . $_POST["aantal-hoofdgerecht-1"] .
-              "<br><b>Duo van tong en zalm, geparfumeerd met saffraan, grijze garnaaltjes, bieslookpuree: </b>" . $_POST["aantal-hoofdgerecht-2"] .
-              "<br><b>Gegrilde tournedos met gemengd slaatje, handgesneden frietjes, saus naar keuze: </b>" . $_POST["aantal-hoofdgerecht-3"] .
-              "<br><b>Stoofpotje van rundvlees met trappist en rozijntjes, verse frietjes: </b>" . $_POST["aantal-hoofdgerecht-4"] .
-
-              "<br><h2>Bestelde kindermenu(s): </h2>" . "<b>Minihamburger met frietjes: </b>" . " " . $_POST["aantal-kindermenu-1"] .
-
-              "<br><h2>Bestelde Pasta's: </h2>" . "<b>Lasagne met gegrilde groenten: </b>" . " " . $_POST["aantal-pasta-1"] .
-              "<br><b>Pasta pesto met huisbereide zuiderse gehaktballetjes met feta en zongedroogde tomaatjes: </b>" . $_POST["aantal-pasta-2"] .
-              "<br><b>Tagliatelle met kip 'Hawaï': </b>" . $_POST["aantal-pasta-3"] .
-
-              "<br><h2>Bestelde Maaltijdsalades: </h2>" . "<b>Salade met kippenreepjes in een krokant jasje geparfumeerd met een zuiderse mangodressing: </b>" . " " . $_POST["aantal-maaltijdsalades-1"] .
-              "<br><b>Lentepluksels met knapzakje van geitenkaas, krokante spekjes en Granny Smith: </b>" . $_POST["aantal-maaltijdsalades-2"] .
-              "<br><b>Slaatje met gebakken kippenlevertjes, spekjes en croutons met een lichte toets van frambozen: </b>" . $_POST["aantal-maaltijdsalades-3"] .
-              "<br><b>Verfrissend slaatje met scampi en appel met een currydressing: </b>" . $_POST["aantal-maaltijdsalades-4"] .
-
-              "<br><h2>Bestelde Desserts: </h2>" . "<b>Tiramisu met speculoos: </b>" . " " . $_POST["aantal-desserts-1"] .
-              "<br><b>Chocolademoussetaartje: </b>" . $_POST["aantal-desserts-2"] .
-              "<br><b>Gevarieerd dessertbord 'Keukenrebellen': </b>" . $_POST["aantal-desserts-3"] .
-
-              "<hr>" .
-
-              "<br><h2>Totaal bedrag: </h2>" . " " . $_POST["totaal"] . "<br><h2>Betalingswijze: </h2>" . " " . $_POST["betaling"];
-*/
 
       $mail->Subject = 'Bestelling van' . " " . $_POST["naam"] . " " . $_POST["voornaam"];
       $mail->Body    = $body;
