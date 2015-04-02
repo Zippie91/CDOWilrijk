eacffe694b2b70d635bf206ca9478c782ec8a09a
lightlessfilms <lightlessfilms@gmail.com>
2015-04-02 15:03:45 +0200

    changed jumbotron text

--- _config.php
+++ _config.php
@@ -1,104 +1,104 @@
 <?php
 $soepen = array(
   array(
-    "gerecht" => "Wortel/kokossoepje met curry en koriander",
-    "prijs" => "3.00"
+    'gerecht' => 'Wortel/kokossoepje met curry en koriander',
+    'prijs' => '3.00'
   ),
   array(
-    "gerecht" => "Basilicumsoepje met tartaar van mozzarella en tomaat",
-    "prijs" => "3.00"
+    'gerecht' => 'Basilicumsoepje met tartaar van mozzarella en tomaat',
+    'prijs' => '3.00'
   ),
   array(
-    "gerecht" => "Tomatenvelouté met balletjes",
-    "prijs" => "3.00"
+    'gerecht' => 'Tomatenvelouté met balletjes',
+    'prijs' => '3.00'
   )
 );
 
 $voorgerechten = array(
   array(
-    "gerecht" => "Carpaccio van sint-jacobsvruchten met tuinkruiden en limoendressing",
-    "prijs" => "7.50"
+    'gerecht' => 'Carpaccio van sint-jacobsvruchten met tuinkruiden en limoendressing',
+    'prijs' => '7.50'
   ),
   array(
-    "gerecht" => "Huisgemaakte geitenkaaskroketjes met een fris slaatje",
-    "prijs" => "7.50"
+    'gerecht' => 'Huisgemaakte geitenkaaskroketjes met een fris slaatje',
+    'prijs' => '7.50'
   ),
   array(
-    "gerecht" => "Vers geplukte lenteblaadjes met gevulde zalmbonbon",
-    "prijs" => "7.50"
+    'gerecht' => 'Vers geplukte lenteblaadjes met gevulde zalmbonbon',
+    'prijs' => '7.50'
   )
 );
 
 $hoofdgerechten = array(
   array(
-    "gerecht" => "Scampi's met appel/ananas/banaan/curry/kokossausje en wilde rijst",
-    "prijs" => "12.50"
+    'gerecht' => 'Scampi\'s met appel/ananas/banaan/curry/kokossausje en wilde rijst',
+    'prijs' => '12.50'
   ),
   array(
-    "gerecht" => "Duo van tong en zalm, geparfumeerd met saffraan, grijze garnaaltjes, bieslookpuree",
-    "prijs" => "12.50"
+    'gerecht' => 'Duo van tong en zalm, geparfumeerd met saffraan, grijze garnaaltjes, bieslookpuree',
+    'prijs' => '12.50'
   ),
   array(
-    "gerecht" => "Gegrilde tournedos met gemengd slaatje, handgesneden frietjes, saus naar keuze (groene peperroom, roze peperroom, archiduc, béarnaise, choron, blackwell, keukenrebellensausje)",
-    "prijs" => "12.50"
+    'gerecht' => 'Gegrilde tournedos met gemengd slaatje, handgesneden frietjes, saus naar keuze (groene peperroom, roze peperroom, archiduc, béarnaise, choron, blackwell, keukenrebellensausje)',
+    'prijs' => '12.50'
   ),
   array(
-    "gerecht" => "Stoofpotje van rundvlees met trappist en rozijntjes, verse frietjes",
-    "prijs" => "10.00"
+    'gerecht' => 'Stoofpotje van rundvlees met trappist en rozijntjes, verse frietjes',
+    'prijs' => '10.00'
   )
 );
 
 $kindermenu = array(
   array(
-    "gerecht" => "Minihamburger met frietjes",
-    "prijs" => "6.50"
+    'gerecht' => 'Minihamburger met frietjes',
+    'prijs' => '6.50'
   )
 );
 
 $pasta = array(
   array(
-    "gerecht" => "Lasagne met gegrilde groenten",
-    "prijs" => "7.50"
+    'gerecht' => 'Lasagne met gegrilde groenten',
+    'prijs' => '7.50'
   ),
   array(
-    "gerecht" => "Pasta pesto met huisbereide zuiderse gehaktballetjes met feta en zongedroogde tomaatjes",
-    "prijs" => "7.50"
+    'gerecht' => 'Pasta pesto met huisbereide zuiderse gehaktballetjes met feta en zongedroogde tomaatjes',
+    'prijs' => '7.50'
   ),
   array(
-    "gerecht" => "Tagliatelle met kip 'Hawaï'",
-    "prijs" => "7.50"
+    'gerecht' => 'Tagliatelle met kip \'Hawaï\'',
+    'prijs' => '7.50'
   )
 );
 $maaltijdsalades = array(
   array(
-    "gerecht" => "Salade met kippenreepjes in een krokant jasje geparfumeerd met een zuiderse mangodressing",
-    "prijs" => "8.50"
+    'gerecht' => 'Salade met kippenreepjes in een krokant jasje geparfumeerd met een zuiderse mangodressing',
+    'prijs' => '8.50'
   ),
   array(
-    "gerecht" => "Lentepluksels met knapzakje van geitenkaas, krokante spekjes en Granny Smith",
-    "prijs" => "8.50"
+    'gerecht' => 'Lentepluksels met knapzakje van geitenkaas, krokante spekjes en Granny Smith',
+    'prijs' => '8.50'
   ),
   array(
-    "gerecht" => "Slaatje met gebakken kippenlevertjes, spekjes en croutons met een lichte toets van frambozen",
-    "prijs" => "8.50"
+    'gerecht' => 'Slaatje met gebakken kippenlevertjes, spekjes en croutons met een lichte toets van frambozen',
+    'prijs' => '8.50'
   ),
   array(
-    "gerecht" => "Verfrissend slaatje met scampi en appel met een currydressing",
-    "prijs" => "8.50"
+    'gerecht' => 'Verfrissend slaatje met scampi en appel met een currydressing',
+    'prijs' => '8.50'
   )
 );
 $desserts = array(
   array(
-    "gerecht" => "Tiramisu met speculoos",
-    "prijs" => "4.50"
+    'gerecht' => 'Tiramisu met speculoos',
+    'prijs' => '4.50'
   ),
   array(
-    "gerecht" => "Chocolademoussetaartje",
-    "prijs" => "4.50"
+    'gerecht' => 'Chocolademoussetaartje',
+    'prijs' => '4.50'
   ),
   array(
-    "gerecht" => "Gevarieerd dessertbord 'Keukenrebellen'",
-    "prijs" => "6.00"
+    'gerecht' => 'Gevarieerd dessertbord \'Keukenrebellen\'',
+    'prijs' => '6.00'
   )
 );
 ?>
