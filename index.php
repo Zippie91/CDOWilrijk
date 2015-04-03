6154cf556bd8d069913dc6822f30a9226000d625
Raf Smits <raf_smits@hotmail.com>
2015-04-03 09:59:40 +0200

    Change mail system for donboscowilrijk.be domain

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Diner || CDO Don Bosco Wilrijk</title>
+    <title>Don Bosco Wilrijk - De Keukenrebellen</title>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="CSS/jumbotron_narrow.css">
@@ -20,12 +20,12 @@
   <body>
     <div class="container">
       <header class="header clearfix">
-        <h1>CDO Don Bosco Wilrijk</h1>
+        <h1>Don Bosco Wilrijk</h1>
         <a href="https://www.facebook.com/events/972216926129512/"><img src="Images/banneractiviteit.png" /></a>
       </header>
       <div class="jumbotron">
-        <h1>Diner Don Bosco</h1>
-        <p class="lead">Naar aanleiding van de opendeurdag kan je 's avonds komen dineren. De studenten van de keuken bereiden voor jullie een geweldig feestmaal!</p>
+        <h1>Don Bosco<br >De Keukenrebellen</h1>
+        <p class="lead">Naar aanleiding van de opendeurdag kan je 's middags komen eten. De studenten van de keuken bereiden voor jullie een geweldig maal!</p>
         <a href="#form" id="navform" class="btn btn-lg btn-success">Bestel Hier! <i class="glyphicon glyphicon-chevron-down"></i></a>
       </div>
       <div class="row">
