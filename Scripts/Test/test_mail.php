f49f3317882045fdc2cfee0a1da082f1ed984954
Raf Smits <raf_smits@hotmail.com>
2015-04-27 13:10:15 +0200

    Change test Mailer

--- Scripts/Test/test_mail.php
+++ Scripts/Test/test_mail.php
@@ -1,3 +1,14 @@
 <?php
-mail('raf_smits@hotmail.com', 'Test mail', 'Dit is een test mailtje');
+$headers = "From: " . strip_tags('rafsmits@gmail.com') . "\r\n";
+$headers .= "Reply-To: ". strip_tags('rafsmits@gmail.com') . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+$subject = 'Test mail 2';
+
+$receiver = 'raf_smits@hotmail.com';
+
+$content = 'Dit is nog eens een test mail.';
+
+mail($receiver, $subject, $content, $headers);
 ?>
