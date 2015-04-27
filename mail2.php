62494a197bc26001be4aa3f775cd9a015e57289f
Raf Smits <raf_smits@hotmail.com>
2015-04-27 15:19:41 +0200

    Cleanup script with error and success messages

--- mail2.php
+++ mail2.php
@@ -10,14 +10,10 @@ $receiver = strip_tags($sender);
 
 include('mailbody.php');
 
-mail($receiver, $subject, $body, $headers);
-
-/*
 if(mail($receiver, $subject, $body, $headers)) {
-  echo '<div class='alert alert-success'>Email verzonden! Als je de email niet aangekregen hebt, <b>kijk dan in je spam-folder of junk mail</b>. <br>Als je hem nog niet vindt, neem dan contact op met Christian Rutges op het nummer 0495/03.75.79.</div>';
+  echo '<div class='alert alert-success'>Email verzonden! Binnenkort krijg je een bevestiging van jouw bestelling. Heb je dit nog niet gekregen na een verloop van tijd, neem dan contact op met Christian Rutges op het nummer 0495/03.75.79.</div>';
 } else {
-  print_r(error_get_last());
   echo '<div class="alert alert-danger">De e-mail is niet verzonden! Probeer opnieuw of neem contact op met Christian Rutges op het nummer 0495/03.75.79.</div>';
 }
-*/
+
 ?>
