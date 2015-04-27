729983be23c3416029eca34a840cd30f46cfc142
Raf Smits <raf_smits@hotmail.com>
2015-04-27 13:32:59 +0200

    Change Mailer to standard php mail()

--- mail2.php
+++ mail2.php
@@ -8,7 +8,9 @@ $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 $subject = 'Bestelling van ' . $_POST["voornaam"] . " " . $_POST["naam"];
 
-$receiver = $_POST["email"];
+$receiver = strip_tags($_POST["email"]);
+
+$body = 'Dit is een test mailtje';
 
 if(mail($receiver, $subject, $body, $headers)) {
   echo '<div class="alert alert-success">Email verzonden! Als je de email niet aangekregen hebt, <b>kijk dan in je spam-folder of junk mail</b>. <br>Als je hem nog niet vindt, neem dan contact op met Christian Rutges op het nummer 0495/03.75.79.</div>';
