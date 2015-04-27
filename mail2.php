26af6a20d35aadd26d98986900002e996ed50802
Raf Smits <raf_smits@hotmail.com>
2015-04-27 13:42:37 +0200

    Change Mailer to standard php mail()

--- mail2.php
+++ mail2.php
@@ -12,11 +12,14 @@ $receiver = strip_tags($_POST["email"]);
 
 $body = 'Dit is een test mailtje';
 
+mail($receiver, $subject, $body, $headers);
+
+/*
 if(mail($receiver, $subject, $body, $headers)) {
   echo '<div class="alert alert-success">Email verzonden! Als je de email niet aangekregen hebt, <b>kijk dan in je spam-folder of junk mail</b>. <br>Als je hem nog niet vindt, neem dan contact op met Christian Rutges op het nummer 0495/03.75.79.</div>';
 } else {
   print_r(error_get_last());
   echo '<div class="alert alert-danger">De e-mail is niet verzonden! Probeer opnieuw of neem contact op met Christian Rutges op het nummer 0495/03.75.79.</div>';
 }
-
+*/
 ?>
