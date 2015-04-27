af7d3661247ffc72aeba7be62e28540a2b9db72c
Raf Smits <raf_smits@hotmail.com>
2015-04-27 11:43:02 +0200

    Add phpinfo script

---
+++ Scripts/Test/Test_ports.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = 'donboscowilrijk.be';
+$ports = array(21, 25, 80, 81, 110, 443, 465, 587, 3306);
+
+foreach ($ports as $port)
+{
+    $connection = @fsockopen($host, $port);
+
+    if (is_resource($connection))
+    {
+        echo '<h2>' . $host . ':' . $port . ' ' . '(' . getservbyport($port, 'tcp') . ') is open.</h2>' . "\n";
+
+        fclose($connection);
+    }
+
+    else
+    {
+        echo '<h2>' . $host . ':' . $port . ' is not responding.</h2>' . "\n";
+    }
+}
+
+?>
