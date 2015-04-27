3d27092d73634499c99ef37efa91fb6d698d7e67
Raf Smits <raf_smits@hotmail.com>
2015-04-27 14:11:28 +0200

    Add headers to mail

--- mail2.php
+++ mail2.php
@@ -1,6 +1,6 @@
 <?php
-$headers = 'From: ' . strip_tags($sender) . ' \r\n';
-$headers .= 'Cc: ' . strip_tags($sender) . ' \r\n';
+$headers = 'From: ' . strip_tags($sender) . " \r\n";
+$headers .= 'Cc: ' . strip_tags($sender) . " \r\n";
 $headers .= 'MIME-Version: 1.0\r\n';
 $headers .= 'Content-Type: text/html; charset=ISO-8859-1\r\n';
 
